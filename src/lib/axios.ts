11d5b6cd6f80501335c85f15ea7c2652b033b534
Syed Abbuzar <syedabbuzar777@gmail.com>
2026-02-25 01:59:09 +0530

    add vercel file

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://sadik-traders-backend.vercel.app/api',
 });
-
+  
 api.interceptors.request.use((config) => {
   if (config.method !== 'get' && !(config.data instanceof FormData)) {
     config.headers['Content-Type'] = 'application/json';
